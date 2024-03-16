6f77441e963b4938e4232aab020dc590948401e7
Justin McElroy <jwmcelr@gmail.com>
2024-03-16 12:24:25 -0400

    Create Menu, MenuItem Value Objects. Create BasicMenuService with hardcoded data

---
+++ MenuApi/Domains/Menus/Menu.cs
@@ -0,0 +1,21 @@
+﻿namespace MenuApi.Domains.Menus
+{
+    /// <summary>
+    /// Represents a Restaurant Menu
+    /// </summary>
+    public class Menu
+    {
+
+        public int Id { get; init; }
+        public List<MenuItem> Items { get; set; }
+        public Menu()
+        {
+            this.Items = new List<MenuItem>();
+        }
+
+        public void addMenuItem(MenuItem item)
+        {
+            this.Items.Add(item);
+        }
+    }
+}
---
+++ MenuApi/Domains/Menus/MenuItem.cs
@@ -0,0 +1,20 @@
+﻿namespace MenuApi.Domains.Menus
+{
+    public class MenuItem
+    {
+        public int Id { get; }
+
+        public string Name { get; set; }
+
+        public string Description { get; set; }
+
+        public MenuItem() { }
+
+        public MenuItem(int id, string name, string description)
+        {
+            this.Id = id;
+            this.Name = name;
+            this.Description = description;
+        }
+    }
+}
