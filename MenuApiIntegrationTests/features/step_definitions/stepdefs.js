0d6869506d01c65c1709d242c2ba64c41fd8e5b3
Justin McElroy <jwmcelr@gmail.com>
2024-05-04 14:34:20 -0400

    Add MenuApiIntegrationTests

---
+++ MenuApiIntegrationTests/features/step_definitions/stepdefs.js
@@ -0,0 +1,27 @@
+const assert = require('assert');
+const { Given, When, Then } = require('@cucumber/cucumber');
+const { spec } = require('pactum');
+
+Given('the menu is available', async function () {
+    // Write code here that turns the phrase above into concrete actions
+    // return 'pending';
+    await spec()
+        .get('https://localhost:7235/api/Menu/1')
+        .withCore({ rejectUnauthorized: false })
+        .expectStatus(200)
+        .expectJsonLike({
+            "id": 1,
+            "items": [
+                {
+                    "id": 1,
+                    "name": "Spaghetti",
+                    "description": "A Delicious Noodly Dish with Spaghetti Sauce and Meatballs"
+                },
+                {
+                    "id": 2,
+                    "name": "Lasagna",
+                    "description": "A plateful of Cheesy, Meaty Deliciousness"
+                }
+            ]
+        });
+});
