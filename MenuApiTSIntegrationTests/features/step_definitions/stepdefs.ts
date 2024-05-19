789af4cafb030fb97d003d8f5d1d9fcb38cd6cc7
Justin McElroy <jwmcelr@gmail.com>
2024-05-19 17:52:45 -0400

    Add basic Serenity test and change Feature to call it

--- MenuApiTSIntegrationTests/features/step_definitions/stepdefs.ts
+++ MenuApiTSIntegrationTests/features/step_definitions/stepdefs.ts
@@ -1,5 +1,10 @@
 import { Given, Then, When } from '@cucumber/cucumber';
 import { spec } from 'pactum';
+import { Ensure, equals } from '@serenity-js/assertions'
+import { actorCalled, Cast, engage } from '@serenity-js/core'
+import { CallAnApi, GetRequest, LastResponse, Send } from '@serenity-js/rest'
+
+const baseURL = 'https://localhost:7235/api/Menu/'
 
 Given('the menu is available', async function () {
     // Write code here that turns the phrase above into concrete actions
@@ -23,4 +28,19 @@ Given('the menu is available', async function () {
                 }
             ]
         });
+});
+
+Given('the playwright menu is available', async function () {
+    // Write code here that turns the phrase above into concrete actions
+    // return 'pending';
+   
+        engage(Cast.where(actor =>
+            actor.whoCan(CallAnApi.at(baseURL)))                  // 1) Add ability
+        )
+    
+
+    await actorCalled('Apisitt').attemptsTo(
+        Send.a(GetRequest.to('1')),             // 2) Send request
+        Ensure.that(LastResponse.status(), equals(200)),  // 3) Verify response
+    )
 });
